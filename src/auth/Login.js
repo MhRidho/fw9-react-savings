698c1f3984ef72e4f3ccfd06711d14498cbf6147
MhRidho <bakarudin947@gmail.com>
2023-03-14 10:54:17 +0700

    add hidden password

--- src/auth/Login.js
+++ src/auth/Login.js
@@ -1,36 +1,46 @@
-import { React, useEffect } from 'react';
-import '../assets/css/styles.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Formik } from 'formik';
-import * as Yup from 'yup';
-import { Container, Row, Col, InputGroup, Form, Stack, Button, Alert } from 'react-bootstrap';
-import { Helmet } from 'react-helmet';
-import { FiMail, FiLock } from 'react-icons/fi';
-import { Link, useNavigate } from 'react-router-dom';
-import WallAuth from '../components/WallAuth';
-import { login } from '../redux/asyncActions/auth';
-import { useDispatch, useSelector } from 'react-redux';
+import { React, useEffect, useState } from "react";
+import "../assets/css/styles.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import {
+  Container,
+  Row,
+  Col,
+  InputGroup,
+  Form,
+  Stack,
+  Button,
+  Alert,
+} from "react-bootstrap";
+import { Helmet } from "react-helmet";
+import { FiMail, FiLock, FiEye, FiEyeOff } from "react-icons/fi";
+import { Link, useNavigate } from "react-router-dom";
+import WallAuth from "../components/WallAuth";
+import { login } from "../redux/asyncActions/auth";
+import { useDispatch, useSelector } from "react-redux";
 
 const loginSchema = Yup.object().shape({
-  email: Yup.string().email('Invalid email address format').required(),
-  password: Yup.string()
-    .required()
-    .min(6, 'Should more than 6 characters')
+  email: Yup.string().email("Invalid email address format").required(),
+  password: Yup.string().required().min(6, "Should more than 6 characters"),
   // .matches(/[a-z]/g, 'Should content at least 1 lowercase')
   // .matches(/[A-Z]/g, 'Should contain at least 1 uppercase')
   // .matches(/[0-9]/g, 'Should contain at least 1 number')
   // .matches(/^\S*$/, 'Should not contain spaces')
-})
+});
 
 const LoginForm = (props) => {
+  const [open, setOpen] = useState(true);
   const successMsg = useSelector((state) => state.auth.successMsg);
   const errorMsg = useSelector((state) => state.auth.errorMsg);
   return (
     <Form noValidate onSubmit={props.handleSubmit}>
-      {successMsg && <Alert variant='success'>{successMsg}</Alert>}
-      {errorMsg && <Alert variant='danger'>{errorMsg}</Alert>}
+      {successMsg && <Alert variant="success">{successMsg}</Alert>}
+      {errorMsg && <Alert variant="danger">{errorMsg}</Alert>}
       <InputGroup className="mb-5">
-        <InputGroup.Text id="basic-addon1" className='inp-logo'><FiMail className='color-A9A9A999 fs-22px' /></InputGroup.Text>
+        <InputGroup.Text id="basic-addon1" className="inp-logo">
+          <FiMail className="color-A9A9A999 fs-22px" />
+        </InputGroup.Text>
         <Form.Control
           type="text"
           name="email"
@@ -41,12 +51,16 @@ const LoginForm = (props) => {
           placeholder="Enter your e-mail"
           isInvalid={!!props.errors.email}
         />
-        <Form.Control.Feedback type='invalid'>{props.errors.email}</Form.Control.Feedback>
+        <Form.Control.Feedback type="invalid">
+          {props.errors.email}
+        </Form.Control.Feedback>
       </InputGroup>
       <InputGroup className="mb-3">
-        <InputGroup.Text id="basic-addon1" className='inp-logo'><FiLock className='color-A9A9A999 fs-22px' /></InputGroup.Text>
+        <InputGroup.Text id="basic-addon1" className="inp-logo">
+          <FiLock className="color-A9A9A999 fs-22px" />
+        </InputGroup.Text>
         <Form.Control
-          type="password"
+          type={open ? "password" : "text"}
           name="password"
           value={props.values.password}
           onChange={props.handleChange}
@@ -54,20 +68,35 @@ const LoginForm = (props) => {
           placeholder="Enter your password"
           isInvalid={!!props.errors.password}
         />
-        <Form.Control.Feedback type='invalid'>{props.errors.password}</Form.Control.Feedback>
+        <InputGroup.Text
+          id="basic-addon1"
+          className="inp-logo"
+          onClick={() => setOpen(!open)}
+        >
+          {open ? (
+            <FiEye className="color-A9A9A999 fs-22px" />
+          ) : (
+            <FiEyeOff className="color-A9A9A999 fs-22px" />
+          )}
+        </InputGroup.Text>
+        <Form.Control.Feedback type="invalid">
+          {props.errors.password}
+        </Form.Control.Feedback>
       </InputGroup>
       <Stack direction="horizontal" gap={3}>
-        <div className="ms-auto mt-3 pb-5"><Link className="a-menu fs-14px fw-600" to={'/reset-password'}>Forgot password?</Link></div>
+        <div className="ms-auto mt-3 pb-5">
+          <Link className="a-menu fs-14px fw-600" to={"/reset-password"}>
+            Forgot password?
+          </Link>
+        </div>
       </Stack>
       <div className="d-grid gap-2 pt-5">
-        <Button className='btn btn-secondary btn-lg ent-email' type='submit'>
+        <Button className="btn btn-secondary btn-lg ent-email" type="submit">
           Login
         </Button>
       </div>
-
     </Form>
   );
-
 };
 
 const Login = () => {
@@ -81,7 +110,7 @@ const Login = () => {
   };
   useEffect(() => {
     if (token) {
-      navigate('/home');
+      navigate("/home");
     }
   }, [navigate, token]);
 
@@ -96,32 +125,39 @@ const Login = () => {
         <Row>
           <WallAuth />
 
-          <Col md={5} >
+          <Col md={5}>
             <Row className="mx-4">
               <Col md={10}>
                 <div>
-                  <h1 className="martop-120px fs-24px fw-700 line-height-32px color-3A3D42">Start Accessing Banking Needs<br />
-                    With All Devices and All Platforms
-                    With 30.000+ Users</h1>
+                  <h1 className="martop-120px fs-24px fw-700 line-height-32px color-3A3D42">
+                    Start Accessing Banking Needs
+                    <br />
+                    With All Devices and All Platforms With 30.000+ Users
+                  </h1>
                 </div>
                 <div>
                   <p className="martop-30px fs-16px color-3A3D4299 line-height-30px">
                     Transfering money is eassier than ever, you can access
-                    Zwallet wherever you are. Desktop,
-                    laptop, mobile phone?
-                    we cover all of that for you!
+                    Zwallet wherever you are. Desktop, laptop, mobile phone? we
+                    cover all of that for you!
                   </p>
                 </div>
 
                 <div className="mt-5">
-
-                  <Formik validationSchema={loginSchema} onSubmit={onLogin} initialValues={{ email: '', password: '' }} >
+                  <Formik
+                    validationSchema={loginSchema}
+                    onSubmit={onLogin}
+                    initialValues={{ email: "", password: "" }}
+                  >
                     {(props) => <LoginForm {...props} />}
                   </Formik>
 
                   <div className="form-check form-check-reverse position-relative text-center my-5">
-                    <label className="form-check-label" >
-                      Don't have an account? Let's <Link className='have-signup' to={"/sign-up"}>Sign Up</Link>
+                    <label className="form-check-label">
+                      Don't have an account? Let's{" "}
+                      <Link className="have-signup" to={"/sign-up"}>
+                        Sign Up
+                      </Link>
                     </label>
                   </div>
                 </div>
@@ -132,8 +168,7 @@ const Login = () => {
       </Container>
       {/* END HOME LOGIN */}
     </>
-  )
-}
-
+  );
+};
 
 export default Login;
